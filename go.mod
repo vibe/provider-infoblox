fd8933989cc49250e142dd856185968ad2f7b8fc
Christian Franco <christian.franco@nbcuni.com>
2024-10-02 04:25:21 -0700

    use vibe/infoblox provider source and github/vibe/terraform-provider-infoblox

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/upbound/upjet-provider-template
+module github.com/vibe/provider-infoblox
 
 go 1.21
 
--- internal/controller/zz_setup.go
+++ internal/controller/zz_setup.go
@@ -9,16 +9,46 @@ import (
 
 	"github.com/crossplane/upjet/pkg/controller"
 
-	resource "github.com/upbound/upjet-provider-template/internal/controller/null/resource"
-	providerconfig "github.com/upbound/upjet-provider-template/internal/controller/providerconfig"
+	record "github.com/vibe/provider-infoblox/internal/controller/a/record"
+	recordaaaa "github.com/vibe/provider-infoblox/internal/controller/aaaa/record"
+	recordcname "github.com/vibe/provider-infoblox/internal/controller/cname/record"
+	view "github.com/vibe/provider-infoblox/internal/controller/dns/view"
+	allocation "github.com/vibe/provider-infoblox/internal/controller/ip/allocation"
+	association "github.com/vibe/provider-infoblox/internal/controller/ip/association"
+	network "github.com/vibe/provider-infoblox/internal/controller/ipv4/network"
+	networkcontainer "github.com/vibe/provider-infoblox/internal/controller/ipv4/networkcontainer"
+	networkipv6 "github.com/vibe/provider-infoblox/internal/controller/ipv6/network"
+	networkcontaineripv6 "github.com/vibe/provider-infoblox/internal/controller/ipv6/networkcontainer"
+	recordmx "github.com/vibe/provider-infoblox/internal/controller/mx/record"
+	viewnetwork "github.com/vibe/provider-infoblox/internal/controller/network/view"
+	providerconfig "github.com/vibe/provider-infoblox/internal/controller/providerconfig"
+	recordptr "github.com/vibe/provider-infoblox/internal/controller/ptr/record"
+	recordsrv "github.com/vibe/provider-infoblox/internal/controller/srv/record"
+	recordtxt "github.com/vibe/provider-infoblox/internal/controller/txt/record"
+	auth "github.com/vibe/provider-infoblox/internal/controller/zone/auth"
 )
 
 // Setup creates all controllers with the supplied logger and adds them to
 // the supplied manager.
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
-		resource.Setup,
+		record.Setup,
+		recordaaaa.Setup,
+		recordcname.Setup,
+		view.Setup,
+		allocation.Setup,
+		association.Setup,
+		network.Setup,
+		networkcontainer.Setup,
+		networkipv6.Setup,
+		networkcontaineripv6.Setup,
+		recordmx.Setup,
+		viewnetwork.Setup,
 		providerconfig.Setup,
+		recordptr.Setup,
+		recordsrv.Setup,
+		recordtxt.Setup,
+		auth.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
